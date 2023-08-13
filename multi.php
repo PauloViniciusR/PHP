0b4bb6cf8b4320e99ab1c632b842fc8673e836c1
Paulo Vinicius R <vinicrf81@gmail.com>
2023-08-13 13:50:51 -0300

    required

---
+++ multi.php
@@ -0,0 +1,14 @@
+<html>
+    <?php 
+
+        $arr = ['Paulo'. 'Vinicius', 'Sabrina'];
+        $arr = array('Gabriella', 'Bruna', 'Jim');
+
+        $arr[0] = 'guilherme';
+        $arr[] = 'joão';
+
+        $arr2 = array(array('Paulo', 'Clara'), array(23, 19));
+
+        echo $arr2 = [0];
+    ?>
+</html>
